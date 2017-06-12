7b080f7d5567ba62e8edca4817c9180ee45fe760
manabuyasuda <info@manabuyasuda.com>
2017-06-12 13:12:27 +0900

    [#42] 要素の高さを揃えるModuleを追加

---
+++ develop/assets/js/module/js/alignHeight.js
@@ -0,0 +1,17 @@
+$(function() {
+
+  /**
+   * 同じ行にある要素の高さを揃えます。
+   * https://github.com/liabru/jquery-match-height#options
+   * 例：
+   * <div class="parent">
+   *   <div class="child js-alignHeight"></div>
+   *   <div class="child js-alignHeight"></div>
+   * </div>
+   */
+  $('.js-alignHeight').matchHeight();
+
+}());
+
+
+
