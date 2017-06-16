3533d848a38f2550c086f595ae073d6cc2afbc10
manabuyasuda <info@manabuyasuda.com>
2017-06-16 18:43:24 +0900

    [#42] 国際電話（+）や空白文字（半角スペースなど）に対応

--- develop/assets/js/module/js/changeTelLink.js
+++ develop/assets/js/module/js/changeTelLink.js
@@ -2,22 +2,22 @@ $(function() {
 
   /**
    * スマホで閲覧している場合にだけ電話番号にリンクを貼ります。
-   * 要素内の文字列から数値だけを取得して、リンクに設定します。
+   * 要素内の文字列からハイフンとスペースなどの空白文字を削除してリンクに設定します。
    * 例：
    * <p>
-   *   <span class="js-changePhoneLink">0120-123-456</span>
+   *   <span class="js-changeTelLink"> +0120-123-456</span>
    * </p>
    *
    * 処理後：
    * <p>
-   *   <span class="js-changePhoneLink"><a href="tel:0120123456">0120-123-456</a></span>
+   *   <span class="js-changeTelLink"><a href="tel:+0120123456"> +0120-123-456</a></span>
    * </p>
    */
   if(isSP) {
     $('.js-changeTelLink').each(function(){
       var $this = $(this);
       var str = $this.text();
-      $this.html($('<a>').attr('href', 'tel:' + str.replace(/[^0-9]+/g, '')).append(str + '</a>'));
+      $this.html($('<a>').attr('href', 'tel:' + str.replace(/[\-\s]/g, '')).append(str + '</a>'));
     });
   }
 
