a0f1e791745a3219eab8e8a42beeafe6d839af70
manabuyasuda <info@manabuyasuda.com>
2017-08-20 22:10:18 +0900

    [#67] JSからのブレイクポイントの参照用に`none`を追加

--- develop/assets/js/lib/breakpoint.js
+++ develop/assets/js/lib/breakpoint.js
@@ -1,10 +1,10 @@
 /**
  * @desc - `body:before`に指定したブレイクポイントを取得します。
  * @link - https://www.lullabot.com/articles/importing-css-breakpoints-into-javascript
- * @example: ブレイクポイント`md`以上の場合
- * if(breakpoint.value === 'md' || breakpoint.value === 'lg') {}
- * @example: ブレイクポイント`lg`以上の場合
- * if(breakpoint.value === 'lg') {}
+ * @example: ブレイクポイント1px以上の場合
+ * if(breakpoint.value === 'none') {}
+ * @example: ブレイクポイント1px以上もしくは`md`の場合
+ * if(breakpoint.value === 'none' || breakpoint.value === 'md') {}
  */
 var breakpoint = {};
 breakpoint.refreshValue = function () {
