2841c261d39d545635095e7aced98e7b21501ecd
manabuyasuda <info@manabuyasuda.com>
2018-02-27 17:41:20 +0900

    [#100] wicg-focus-ringからfocus-visibleに変更

--- htdocs/assets/js/lib.js
+++ htdocs/assets/js/lib.js
@@ -5,198 +5,32 @@
 }(this, (function () { 'use strict';
 
 /**
- * Module export
- *
- * @param {Element} el
- * @return {ClassList}
- */
-
-var index = function (el) {
-  return new ClassList(el);
-};
-
-/**
- * Initialize a new ClassList for the given element
- *
- * @param {Element} el DOM Element
- */
-function ClassList(el) {
-  if (!el || el.nodeType !== 1) {
-    throw new Error('A DOM Element reference is required');
-  }
-
-  this.el = el;
-  this.classList = el.classList;
-}
-
-/**
- * Check token validity
- *
- * @param token
- * @param [method]
- */
-function checkToken(token, method) {
-  method = method || 'a method';
-
-  if (typeof token != 'string') {
-    throw new TypeError(
-      'Failed to execute \'' + method + '\' on \'ClassList\': ' +
-      'the token provided (\'' + token + '\') is not a string.'
-    );
-  }
-  if (token === "") {
-    throw new SyntaxError(
-      'Failed to execute \'' + method + '\' on \'ClassList\': ' +
-      'the token provided must not be empty.'
-    );
-  }
-  if (/\s/.test(token)) {
-    throw new Error(
-      'Failed to execute \'' + method + '\' on \'ClassList\': ' +
-      'the token provided (\'' + token + '\') contains HTML space ' +
-      'characters, which are not valid in tokens.'
-    );
-  }
-}
-
-/**
- * Return an array of the class names on the element.
- *
- * @return {Array}
- */
-ClassList.prototype.toArray = function () {
-  var str = (this.el.getAttribute('class') || '').replace(/^\s+|\s+$/g, '');
-  var classes = str.split(/\s+/);
-  if ('' === classes[0]) { classes.shift(); }
-  return classes;
-};
-
-/**
- * Add the given `token` to the class list if it's not already present.
- *
- * @param {String} token
- */
-ClassList.prototype.add = function (token) {
-  var classes, index, updated;
-  checkToken(token, 'add');
-
-  if (this.classList) {
-    this.classList.add(token);
-  }
-  else {
-    // fallback
-    classes = this.toArray();
-    index = classes.indexOf(token);
-    if (index === -1) {
-      classes.push(token);
-      this.el.setAttribute('class', classes.join(' '));
-    }
-  }
-
-  return;
-};
-
-/**
- * Check if the given `token` is in the class list.
- *
- * @param {String} token
- * @return {Boolean}
- */
-ClassList.prototype.contains = function (token) {
-  checkToken(token, 'contains');
-
-  return this.classList ?
-    this.classList.contains(token) :
-    this.toArray().indexOf(token) > -1;
-};
-
-/**
- * Remove any class names that match the given `token`, when present.
- *
- * @param {String|RegExp} token
- */
-ClassList.prototype.remove = function (token) {
-  var arr, classes, i, index, len;
-
-  if ('[object RegExp]' == Object.prototype.toString.call(token)) {
-    arr = this.toArray();
-    for (i = 0, len = arr.length; i < len; i++) {
-      if (token.test(arr[i])) {
-        this.remove(arr[i]);
-      }
-    }
-  }
-  else {
-    checkToken(token, 'remove');
-
-    if (this.classList) {
-      this.classList.remove(token);
-    }
-    else {
-      // fallback
-      classes = this.toArray();
-      index = classes.indexOf(token);
-      if (index > -1) {
-        classes.splice(index, 1);
-        this.el.setAttribute('class', classes.join(' '));
-      }
-    }
-  }
-
-  return;
-};
-
-/**
- * Toggle the `token` in the class list. Optionally force state via `force`.
- *
- * Native `classList` is not used as some browsers that support `classList` do
- * not support `force`. Avoiding `classList` altogether keeps this function
- * simple.
- *
- * @param {String} token
- * @param {Boolean} [force]
- * @return {Boolean}
- */
-ClassList.prototype.toggle = function (token, force) {
-  checkToken(token, 'toggle');
-
-  var hasToken = this.contains(token);
-  var method = hasToken ? (force !== true && 'remove') : (force !== false && 'add');
-
-  if (method) {
-    this[method](token);
-  }
-
-  return (typeof force == 'boolean' ? force : !hasToken);
-};
-
-/**
  * https://github.com/WICG/focus-ring
  */
 function init() {
-  var hadKeyboardEvent = false;
-  var elWithFocusRing;
-
+  var hadKeyboardEvent = true;
+  var hadFocusVisibleRecently = false;
+  var hadFocusVisibleRecentlyTimeout = null;
   var inputTypesWhitelist = {
-    'text': true,
-    'search': true,
-    'url': true,
-    'tel': true,
-    'email': true,
-    'password': true,
-    'number': true,
-    'date': true,
-    'month': true,
-    'week': true,
-    'time': true,
-    'datetime': true,
-    'datetime-local': true,
+    text: true,
+    search: true,
+    url: true,
+    tel: true,
+    email: true,
+    password: true,
+    number: true,
+    date: true,
+    month: true,
+    week: true,
+    time: true,
+    datetime: true,
+    'datetime-local': true
   };
 
   /**
    * Computes whether the given element should automatically trigger the
-   * `focus-ring` class being added, i.e. whether it should always match
-   * `:focus-ring` when focused.
+   * `focus-visible` class being added, i.e. whether it should always match
+   * `:focus-visible` when focused.
    * @param {Element} el
    * @return {boolean}
    */
@@ -204,116 +38,192 @@ function init() {
     var type = el.type;
     var tagName = el.tagName;
 
-    if (tagName == 'INPUT' && inputTypesWhitelist[type] && !el.readonly)
+    if (tagName == 'INPUT' && inputTypesWhitelist[type] && !el.readonly) {
       return true;
+    }
 
-    if (tagName == 'TEXTAREA' && !el.readonly)
+    if (tagName == 'TEXTAREA' && !el.readonly) {
       return true;
+    }
 
-    if (el.contentEditable == 'true')
+    if (el.contentEditable == 'true') {
       return true;
+    }
 
     return false;
   }
 
   /**
-   * Add the `focus-ring` class to the given element if it was not added by
+   * Add the `focus-visible` class to the given element if it was not added by
    * the author.
    * @param {Element} el
    */
-  function addFocusRingClass(el) {
-    if (index(el).contains('focus-ring'))
+  function addFocusVisibleClass(el) {
+    if (el.classList.contains('focus-visible')) {
       return;
-    index(el).add('focus-ring');
-    el.setAttribute('data-focus-ring-added', '');
+    }
+    el.classList.add('focus-visible');
+    el.setAttribute('data-focus-visible-added', '');
   }
 
   /**
-   * Remove the `focus-ring` class from the given element if it was not
+   * Remove the `focus-visible` class from the given element if it was not
    * originally added by the author.
    * @param {Element} el
    */
-  function removeFocusRingClass(el) {
-    if (!el.hasAttribute('data-focus-ring-added'))
+  function removeFocusVisibleClass(el) {
+    if (!el.hasAttribute('data-focus-visible-added')) {
       return;
-    index(el).remove('focus-ring');
-    el.removeAttribute('data-focus-ring-added');
+    }
+    el.classList.remove('focus-visible');
+    el.removeAttribute('data-focus-visible-added');
   }
 
   /**
-   * On `keydown`, set `hadKeyboardEvent`, add `focus-ring` class if the
-   * key was Tab.
+   * On `keydown`, set `hadKeyboardEvent`, add `focus-visible` class if the
+   * key was Tab/Shift-Tab or Arrow Keys.
    * @param {Event} e
    */
   function onKeyDown(e) {
-    if (e.altKey || e.ctrlKey || e.metaKey)
+    // Ignore keypresses if the user is holding down a modifier key.
+    if (e.altKey || e.ctrlKey || e.metaKey) {
       return;
-
-    if (e.keyCode != 9)
-      return;
+    }
 
     hadKeyboardEvent = true;
   }
 
   /**
-   * On `focus`, add the `focus-ring` class to the target if:
-   * - the target received focus as a result of keyboard navigation
+   * If at any point a user clicks with a pointing device, ensure that we change
+   * the modality away from keyboard.
+   * This avoids the situation where a user presses a key on an already focused
+   * element, and then clicks on a different element, focusing it with a
+   * pointing device, while we still think we're in keyboard modality.
+   * @param {Event} e
+   */
+  function onPointerDown(e) {
+    hadKeyboardEvent = false;
+  }
+
+  /**
+   * On `focus`, add the `focus-visible` class to the target if:
+   * - the target received focus as a result of keyboard navigation, or
    * - the event target is an element that will likely require interaction
    *   via the keyboard (e.g. a text box)
    * @param {Event} e
    */
   function onFocus(e) {
-    if (e.target == document)
+    // Prevent IE from focusing the document or HTML element.
+    if (e.target == document || e.target.nodeName == 'HTML') {
       return;
+    }
 
     if (hadKeyboardEvent || focusTriggersKeyboardModality(e.target)) {
-      addFocusRingClass(e.target);
+      addFocusVisibleClass(e.target);
       hadKeyboardEvent = false;
     }
   }
 
   /**
-   * On `blur`, remove the `focus-ring` class from the target.
+   * On `blur`, remove the `focus-visible` class from the target.
    * @param {Event} e
    */
   function onBlur(e) {
-    if (e.target == document)
+    if (e.target == document || e.target.nodeName == 'HTML') {
       return;
+    }
 
-    removeFocusRingClass(e.target);
-  }
-
-  /**
-   * When the window regains focus, restore the focus-ring class to the element
-   * to which it was previously applied.
-   */
-  function onWindowFocus() {
-    if (document.activeElement == elWithFocusRing)
-      addFocusRingClass(elWithFocusRing);
-
-    elWithFocusRing = null;
-  }
-
-  /**
-   * When switching windows, keep track of the focused element if it has a
-   * focus-ring class.
-   */
-  function onWindowBlur() {
-    if (index(document.activeElement).contains('focus-ring')) {
-      // Keep a reference to the element to which the focus-ring class is applied
-      // so the focus-ring class can be restored to it if the window regains
-      // focus after being blurred.
-      elWithFocusRing = document.activeElement;
+    if (e.target.classList.contains('focus-visible')) {
+      // To detect a tab/window switch, we look for a blur event followed
+      // rapidly by a visibility change.
+      // If we don't see a visibility change within 100ms, it's probably a
+      // regular focus change.
+      hadFocusVisibleRecently = true;
+      window.clearTimeout(hadFocusVisibleRecentlyTimeout);
+      hadFocusVisibleRecentlyTimeout = window.setTimeout(function() {
+        hadFocusVisibleRecently = false;
+        window.clearTimeout(hadFocusVisibleRecentlyTimeout);
+      }, 100);
+      removeFocusVisibleClass(e.target);
     }
   }
 
+  /**
+   * If the user changes tabs, keep track of whether or not the previously
+   * focused element had .focus-visible.
+   * @param {Event} e
+   */
+  function onVisibilityChange(e) {
+    if (document.visibilityState == 'hidden') {
+      // If the tab becomes active again, the browser will handle calling focus
+      // on the element (Safari actually calls it twice).
+      // If this tab change caused a blur on an element with focus-visible,
+      // re-apply the class when the user switches back to the tab.
+      if (hadFocusVisibleRecently) {
+        hadKeyboardEvent = true;
+      }
+      addInitialPointerMoveListeners();
+    }
+  }
+
+  /**
+   * Add a group of listeners to detect usage of any pointing devices.
+   * These listeners will be added when the polyfill first loads, and anytime
+   * the window is blurred, so that they are active when the window regains
+   * focus.
+   */
+  function addInitialPointerMoveListeners() {
+    document.addEventListener('mousemove', onInitialPointerMove);
+    document.addEventListener('mousedown', onInitialPointerMove);
+    document.addEventListener('mouseup', onInitialPointerMove);
+    document.addEventListener('pointermove', onInitialPointerMove);
+    document.addEventListener('pointerdown', onInitialPointerMove);
+    document.addEventListener('pointerup', onInitialPointerMove);
+    document.addEventListener('touchmove', onInitialPointerMove);
+    document.addEventListener('touchstart', onInitialPointerMove);
+    document.addEventListener('touchend', onInitialPointerMove);
+  }
+
+  function removeInitialPointerMoveListeners() {
+    document.removeEventListener('mousemove', onInitialPointerMove);
+    document.removeEventListener('mousedown', onInitialPointerMove);
+    document.removeEventListener('mouseup', onInitialPointerMove);
+    document.removeEventListener('pointermove', onInitialPointerMove);
+    document.removeEventListener('pointerdown', onInitialPointerMove);
+    document.removeEventListener('pointerup', onInitialPointerMove);
+    document.removeEventListener('touchmove', onInitialPointerMove);
+    document.removeEventListener('touchstart', onInitialPointerMove);
+    document.removeEventListener('touchend', onInitialPointerMove);
+  }
+
+  /**
+   * When the polfyill first loads, assume the user is in keyboard modality.
+   * If any event is received from a pointing device (e.g. mouse, pointer,
+   * touch), turn off keyboard modality.
+   * This accounts for situations where focus enters the page from the URL bar.
+   * @param {Event} e
+   */
+  function onInitialPointerMove(e) {
+    // Work around a Safari quirk that fires a mousemove on <html> whenever the
+    // window blurs, even if you're tabbing out of the page. ¯\_(ツ)_/¯
+    if (e.target.nodeName.toLowerCase() === 'html') {
+      return;
+    }
+
+    hadKeyboardEvent = false;
+    removeInitialPointerMoveListeners();
+  }
+
   document.addEventListener('keydown', onKeyDown, true);
+  document.addEventListener('mousedown', onPointerDown, true);
+  document.addEventListener('pointerdown', onPointerDown, true);
+  document.addEventListener('touchstart', onPointerDown, true);
   document.addEventListener('focus', onFocus, true);
   document.addEventListener('blur', onBlur, true);
-  window.addEventListener('focus', onWindowFocus, true);
-  window.addEventListener('blur', onWindowBlur, true);
+  document.addEventListener('visibilitychange', onVisibilityChange, true);
+  addInitialPointerMoveListeners();
 
-  index(document.body).add('js-focus-ring');
+  document.body.classList.add('js-focus-visible');
 }
 
 /**
@@ -321,22 +231,23 @@ function init() {
  * @param {Function} callback
  */
 function onDOMReady(callback) {
+  var loaded;
+
+  /**
+   * Callback wrapper for check loaded state
+   */
+  function load() {
+    if (!loaded) {
+      loaded = true;
+
+      callback();
+    }
+  }
+
   if (document.readyState === 'complete') {
     callback();
   } else {
-    var loaded = false;
-
-    /**
-     * Callback wrapper for check loaded state
-     */
-    function load() {
-      if (!loaded) {
-        loaded = true;
-
-        callback();
-      }
-    }
-
+    loaded = false;
     document.addEventListener('DOMContentLoaded', load, false);
     window.addEventListener('load', load, false);
   }
