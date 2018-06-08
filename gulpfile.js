07ab91bd3bfcf3f9261b21200babbba57851b890
manabuyasuda <info@manabuyasuda.com>
2018-06-08 16:08:41 +0900

    [#101] gulp-uglifyのライセンスのコメントを残す指定を修正
    v3.0.0から指定方法が変更になった。

--- gulpfile.js
+++ gulpfile.js
@@ -141,7 +141,7 @@ gulp.task('css', function(){
   }).on('error', sass.logError))
   .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
   .pipe(postcss(plugins))
-  // .pipe(cleanCSS())
+  .pipe(cleanCSS())
   .pipe(sourcemaps.write('.'))
   .pipe(gulp.dest(dest.root))
   .pipe(browserSync.reload({stream: true}));
@@ -156,7 +156,7 @@ gulp.task('libJs', function() {
   // ファイルを連結します。
   .pipe(concat('lib.js'))
   // ファイルを圧縮します。
-  // .pipe(uglify({preserveComments: 'license'}))
+  .pipe(uglify({output: {comments: /^!/}}))
   .pipe(sourcemaps.write('.'))
   .pipe(gulp.dest(dest.js))
   .pipe(browserSync.reload({stream: true}));
@@ -169,7 +169,7 @@ gulp.task('siteJs', function() {
   return gulp.src(src.siteJs)
   .pipe(sourcemaps.init())
   .pipe(concat('site.js'))
-  // .pipe(uglify({preserveComments: 'license'}))
+  .pipe(uglify({output: {comments: /^!/}}))
   .pipe(sourcemaps.write('.'))
   .pipe(gulp.dest(dest.js))
   .pipe(browserSync.reload({stream: true}));
