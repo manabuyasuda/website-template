9a8bb4b5d1f931c4a948c248ca8b07dd104a2e98
manabuyasuda <info@manabuyasuda.com>
2018-09-12 18:44:18 +0900

    [#44] flexibility.jsを追加

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -2,6 +2,7 @@
 
 import 'picturefill';
 import 'focus-visible';
+import 'flexibility';
 
 import jsAlignHeight from './namespace/js/alignHeight';
 import jsSmoothScroll from './namespace/js/smoothScroll';
