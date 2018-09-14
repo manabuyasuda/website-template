1c9400dd0c3938127d06bdee9c9b8c452addf399
manabuyasuda <info@manabuyasuda.com>
2018-09-14 14:26:47 +0900

    [#135] svg4everybodyの実行を追加

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -3,12 +3,13 @@
 import 'picturefill';
 import 'focus-visible';
 import 'flexibility';
-import 'svg4everybody';
+import svg4everybody from 'svg4everybody';
 
 import jsAlignHeight from './namespace/js/alignHeight';
 import jsSmoothScroll from './namespace/js/smoothScroll';
 
 (() => {
+  svg4everybody();
   jsAlignHeight();
   jsSmoothScroll();
 })();
