dd1ba76aa5855ebc01e30f9573eb3be6918283b1
manabuyasuda <info@manabuyasuda.com>
2018-10-16 19:57:54 +0900

    [#146] ポリフィルを追加

---
+++ src/assets/js/polyfill.js
@@ -0,0 +1,6 @@
+import "@babel/polyfill";
+import 'element-closest';
+import 'picturefill';
+import 'flexibility';
+import svg4everybody from 'svg4everybody';
+svg4everybody();
--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -1,20 +1,13 @@
 'use strict';
-import "@babel/polyfill";
-
-import 'picturefill';
+import './polyfill';
 import 'what-input';
-import 'flexibility';
-import svg4everybody from 'svg4everybody';
 
 import jsAlignHeight from './namespace/js/alignHeight';
 import jsSmoothScroll from './namespace/js/smoothScroll';
 import structureBackToTop from './namespace/structure/backToTop';
 import sitewideDialog from './namespace/sitewide/dialog';
 
-(() => {
-  svg4everybody();
-  jsAlignHeight();
-  jsSmoothScroll();
-  structureBackToTop();
-  sitewideDialog();
-})();
+jsAlignHeight();
+jsSmoothScroll();
+structureBackToTop();
+sitewideDialog();
