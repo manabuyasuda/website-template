c1282bb5624d77ac2c12985b8351fad6e4892ca1
manabuyasuda <info@manabuyasuda.com>
2018-10-18 17:06:21 +0900

    object-fitのポリフィルとモジュールを追加

--- src/assets/js/polyfill.js
+++ src/assets/js/polyfill.js
@@ -2,5 +2,6 @@ import "@babel/polyfill";
 import 'element-closest';
 import 'picturefill';
 import 'flexibility';
+import objectFitPolyfill from 'objectFitPolyfill';
 import svg4everybody from 'svg4everybody';
 svg4everybody();
