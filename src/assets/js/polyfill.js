02082755be6f9cdaacc763e89624bdb08fe8bc27
manabuyasuda <info@manabuyasuda.com>
2018-10-18 18:02:44 +0900

    import方法を修正

--- src/assets/js/polyfill.js
+++ src/assets/js/polyfill.js
@@ -2,6 +2,6 @@ import "@babel/polyfill";
 import 'element-closest';
 import 'picturefill';
 import 'flexibility';
-import objectFitPolyfill from 'objectFitPolyfill';
+import 'objectFitPolyfill';
 import svg4everybody from 'svg4everybody';
 svg4everybody();
