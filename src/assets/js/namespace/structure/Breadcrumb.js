ee5ea85f87711ccced8747a2963b38a9c0431c56
manabuyasuda <info@manabuyasuda.com>
2018-10-26 17:41:48 +0900

    Update: パンくずリストにaria-*属性などを指定してアクセシブルにする
    記述漏れを起こしやすく、一括管理しにくいモジュールなので、JS側から一定の範囲で属性などを追加する。
    HTML側で指定があった場合も上書きする。
    
    - `aria-label`の追加。
      - 言語ごとに値を変更する。
      - lang属性がない場合は処理しない。
    - `aria-current`の追加。
      - 値は`page`とする。
    
    #154

---
+++ src/assets/js/namespace/structure/Breadcrumb.js
@@ -0,0 +1,42 @@
+/**
+ * `nav`に`aria-label`を、
+ * 表示中のページと同じURLのリンクに`aria-current`を追加します。
+ * https://www.w3.org/TR/wai-aria-practices/examples/breadcrumb/
+ */
+export default function structureBreadcrumb() {
+  const Selector = {
+    PARRENT: 'st-Breadcrumb',
+    LINK: 'st-Breadcrumb_Link'
+  };
+  const Attr = {
+    CURRENT_VALUE: 'page'
+  }
+  const getLabelValue = (lang) => {
+    if (lang === 'ja') return '現在位置';
+    if (lang == 'en') return 'Current location';
+    if (lang == 'zh-CN') return '目前的位置';
+    if (lang == 'zh-TW') return '目前的位置';
+    return false;
+  };
+
+  const parrent = document.getElementsByClassName(Selector.PARRENT)[0];
+  const link = document.getElementsByClassName(Selector.LINK);
+  const path = location.pathname.replace(/index\.html$/, '');
+  const pageLang = document.getElementsByTagName('html')[0].getAttribute('lang');
+
+  window.addEventListener('load', () => {
+    if (pageLang) {
+      parrent.setAttribute('aria-label', getLabelValue(pageLang));
+    }
+
+    [...link].forEach(item => {
+      const href = item.getAttribute('href').replace(/index\.html$/, '');
+      const isMatchCurrentPage = href === path ? true : false;
+
+      if (isMatchCurrentPage) {
+        item.setAttribute('aria-current', Attr.CURRENT_VALUE);
+        item.setAttribute('tabindex', -1);
+      }
+    });
+  });
+};
--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -6,10 +6,12 @@ import vueTest from './vue';
 import jsAlignHeight from './namespace/js/alignHeight';
 import jsSmoothScroll from './namespace/js/smoothScroll';
 import structureBackToTop from './namespace/structure/backToTop';
+import structureBreadcrumb from './namespace/structure/Breadcrumb';
 import sitewideDialog from './namespace/sitewide/dialog';
 
 vueTest();
 jsAlignHeight();
 jsSmoothScroll();
 structureBackToTop();
+structureBreadcrumb();
 sitewideDialog();
