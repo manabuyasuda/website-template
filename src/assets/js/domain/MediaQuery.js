8226adaceb592478f43d0bc63bb486cff52a928f
manabuyasuda <info@manabuyasuda.com>
2019-08-08 09:25:52 +0900

    Improve: プライベート変数にして上書きできないようにする
    #275

--- src/assets/js/domain/MediaQuery.js
+++ src/assets/js/domain/MediaQuery.js
@@ -1,34 +1,42 @@
 const BROWSER_DEFAULT_FONT_SIZE = 16;
+const list = {
+  sm: 375,
+  md: 768,
+  lg: 1024,
+  xl: 1440,
+};
 
 /**
  * メディアクエリ変数を管理して、`this.match(query, layoutChangedCallback)`で`query`が一致するか判定します。
  * @example
- * import MediaQuery from './MediaQuery';
- * const mediaQuery = new MediaQuery();
+ * import MediaQuery from './domain/MediaQuery';
  */
 export default class MediaQuery {
-  constructor() {
-    this.sm = 375;
-    this.md = 768;
-    this.lg = 1024;
-    this.xl = 1440;
+  /**
+   * メディアクエリ文字列を返します。
+   * @return {Object} list
+   * @example
+   * const mediaQueryList = MediaQuery.list;
+   */
+  static get list() {
+    return list;
   }
 
   /**
    * メディアクエリがロードで判定またはリサイズで判定が切り替わったときに、コールバックで真偽値を返します。
    * @link https://github.com/yuheiy/real-world-website-boilerplate/blob/master/src/js/mediaQuery.js
-   * @param {(String | Number)} query - メディアクエリ文字列（`this.queries`）か数値
+   * @param {(String | Number)} query - メディアクエリ文字列（`list`のキー）か数値
    * @param {boolean} layoutChangedCallback - メディアクエリが一致するかの真偽値
    * @example
-   * mediaQuery.match('md', matches => {
+   * MediaQuery.matches('md', matches => {
    *   console.log(matches ? 'md' : 'sm');
    * });
-   * mediaQuery.match(768, matches => {
+   * MediaQuery.matches(768, matches => {
    *   console.log(matches ? '768以上' : '767以下');
    * });
    */
-  match(query, layoutChangedCallback) {
-    let mediaQuery = `(min-width: ${this[query] / BROWSER_DEFAULT_FONT_SIZE}em)`;
+  static matches(query, layoutChangedCallback) {
+    let mediaQuery = `(min-width: ${list[query] / BROWSER_DEFAULT_FONT_SIZE}em)`;
 
     if (typeof query === 'number') {
       mediaQuery = `(min-width: ${query / BROWSER_DEFAULT_FONT_SIZE}em)`;
