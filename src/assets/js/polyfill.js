1cfa188831a7268386f00642127218643cd8e9f6
安田学 <info@manabuyasuda.com>
2021-10-13 20:26:12 +0900

    Remove: 使用頻度の低いJSを削除
    #312

--- src/assets/js/polyfill.js
+++ src/assets/js/polyfill.js
@@ -1,6 +1,5 @@
 import 'es6-promise/auto';
 import 'picturefill';
-import 'flexibility';
 import 'objectFitPolyfill';
 import elementClosest from 'element-closest';
 import svg4everybody from 'svg4everybody';
--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -2,15 +2,9 @@ import './polyfill';
 import 'what-input';
 import 'inobounce';
 
-import jsAlignHeight from './namespace/js/jsAlignHeight';
 import jsSmoothScroll from './namespace/js/jsSmoothScroll';
 import stBackToTop from './namespace/structure/stBackToTop';
-import stBreadcrumb from './namespace/structure/stBreadcrumb';
-import swDialog from './namespace/sitewide/swDialog';
 
-jsAlignHeight();
 jsSmoothScroll();
 stBackToTop();
-stBreadcrumb();
-swDialog();
 Promise.resolve().then(res => console.log('Promise end'));// eslint-disable-line
